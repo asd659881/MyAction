6d3adf8235fdb4a4f9ef4b7459e3138417bdca2a
asd659881 <64941343+asd659881@users.noreply.github.com>
2021-04-07 22:30:33 +0800

    Update jdPlantBeanShareCodes.js

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -8,8 +8,7 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PlantBeanShareCodes = [
-  '3xd6hi7wz3detzueuft3fjnaq6shonvreqhvugy@nkvdrkoit5o65hgsezt2hkynoeq3olf63v6icua@fb227jqogvovf4dzt65kyksuqq3h7wlwy7o5jii@2glpkm3dt2ujt5eufktgoxeewa5ac3f4ijdgqji@anvpoh7gttncs535ikqc3dfbbsnst3auzw6gmjq@e7lhibzb3zek3l2je2y7rjczlz3sq4c6e2r72di@mlrdw3aw26j3xrwlavyve554fsprq7lxnn2esoa@7ii2tqua5cw4cuvznmvewfo7gbrfz5c4dyurxen5sazkv5ctbrdq',//账号一的好友shareCode,不同好友中间用@符号隔开
-  '3xd6hi7wz3detzueuft3fjnaq6shonvreqhvugy@nkvdrkoit5o65hgsezt2hkynoeq3olf63v6icua@fb227jqogvovf4dzt65kyksuqq3h7wlwy7o5jii@2glpkm3dt2ujt5eufktgoxeewa5ac3f4ijdgqji@anvpoh7gttncs535ikqc3dfbbsnst3auzw6gmjq@e7lhibzb3zek3l2je2y7rjczlz3sq4c6e2r72di@mlrdw3aw26j3xrwlavyve554fsprq7lxnn2esoa@7ii2tqua5cw4cuvznmvewfo7gbrfz5c4dyurxen5sazkv5ctbrdq',//账号二的好友shareCode，不同好友中间用@符号隔开
+  
 ]
 // 判断github action里面是否有种豆得豆互助码
 if (process.env.PLANT_BEAN_SHARECODES) {
@@ -24,8 +23,4 @@ if (process.env.PLANT_BEAN_SHARECODES) {
   }
 } else if (process.env.JD_COOKIE) {
   console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
-}
-for (let i = 0; i < PlantBeanShareCodes.length; i++) {
-  const index = (i + 1 === 1) ? '' : (i + 1);
-  exports['PlantBeanShareCodes' + index] = PlantBeanShareCodes[i];
 }
